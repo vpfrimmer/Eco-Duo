6f22ad26c153227dc73e8bd3ac54f05ce1d419eb
Vivien Pfrimmer <vivien.pfrimmer@gmail.com>
2015-10-09 23:45:51 +0200

    Scène de menu basique

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public string directSceneName = "Game";
+	
+	public void DirectLaunch(string sceneName = null) {
+		Application.LoadLevel((sceneName == null) ? directSceneName : sceneName);
+	}
+}
