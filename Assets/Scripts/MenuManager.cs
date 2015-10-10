f9b36cbcca9813a76eec58d8a40d08059150b12f
Alain <Alain@alain>
2015-10-10 15:41:51 +0200

    Debut ecran titre + credits + regles Ecran titre Credit Regles

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,11 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
 	public string directSceneName = "Game";
-	
-	public void DirectLaunch(string sceneName = null) {
+    public GameObject rulesPanel;
+    public GameObject creditsPanel;
+    private Animator rulesAnimator;
+    private Animator creditsAnimator;
+
+    public void Start()
+    {
+        creditsAnimator = creditsPanel.GetComponent<Animator>();
+        rulesAnimator = rulesPanel.GetComponent<Animator>();
+        rulesPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void DirectLaunch(string sceneName = null) {
 		Application.LoadLevel((sceneName == null) ? directSceneName : sceneName);
 	}
+
+    public void ShowRules()
+    {
+        rulesPanel.SetActive(true);
+        rulesAnimator.SetBool("Open", true);
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+        creditsAnimator.SetBool("Open", true);
+    }
+
+    public void HideRules()
+    {
+        rulesAnimator.SetBool("Open", false);
+    }
+
+    public void HideCredits()
+    {
+        creditsAnimator.SetBool("Open", false);
+    }
 }
